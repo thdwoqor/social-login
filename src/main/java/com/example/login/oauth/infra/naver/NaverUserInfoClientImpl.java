9e49f6b5e24e76ccb52a7093b8f8dd4a00a6b14f
thdwoqor <celebarte01@gmail.com>
2023-09-14 01:59:08 +0900

    feat: RestClient 적용

--- src/main/java/com/example/login/oauth/infra/naver/NaverUserInfoClientImpl.java
+++ src/main/java/com/example/login/oauth/infra/naver/NaverUserInfoClientImpl.java
@@ -2,17 +2,25 @@ package com.example.login.oauth.infra.naver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class NaverUserInfoClientImpl {
 
+//    @Bean
+//    public NaverUserInfoClient naverUserInfoClient() {
+//        WebClient webClient = WebClient.create();
+//        HttpServiceProxyFactory build = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(webClient))
+//                .build();
+//        return build.createClient(NaverUserInfoClient.class);
+//    }
+
     @Bean
     public NaverUserInfoClient naverUserInfoClient() {
-        WebClient webClient = WebClient.create();
-        HttpServiceProxyFactory build = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(webClient))
+        RestClient restClient = RestClient.create();
+        HttpServiceProxyFactory build = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient))
                 .build();
         return build.createClient(NaverUserInfoClient.class);
     }
